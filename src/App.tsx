926ba304c6522a6e8227b244ad14156709952c2c
Alex Elgueta <alexelgueta.developer@gmail.com>
2024-05-13 01:54:56 -0400

    chore: Update npm dependencies for analytics package

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Navbar from "./components/Navbar";
 import Secret from "./components/Secret";
 import Footer from "./components/Footer";
 import LocomotiveScroll from "locomotive-scroll";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   // @ts-expect-error - LocomotiveScroll is not used in this file
@@ -16,6 +17,7 @@ const App = () => {
   if (isMobile) {
     return (
       <div className="w-full min-h-screen bg-zinc-100 text-zinc-800 flex items-center justify-center text-center p-4">
+        <Analytics />
         <div className="bg-white shadow-lg rounded-lg p-8">
           <h1 className="font-bold text-2xl mb-4">En construcción</h1>
           <p>
@@ -30,6 +32,7 @@ const App = () => {
 
   return (
     <div className="w-full min-h-screen bg-zinc-100 text-zinc-800">
+      <Analytics />
       <Navbar />
       <LandingPage />
       <Marquee />
